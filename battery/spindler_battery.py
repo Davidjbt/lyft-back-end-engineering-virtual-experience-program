42c8f3145de2418d23be07d6d11b5ed53968f160
David <davidbazanticse@gmail.com>
2023-01-06 16:20:12 +0000

    Task 2: Refactoring, done.

---
+++ battery/spindler_battery.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from battery.battery import Battery
+
+class SpindlerBattery(Battery):
+
+    def __init__(self, last_service_date: datetime, current_date: datetime):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        return self.current_date > service_threshold_date
